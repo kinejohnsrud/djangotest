4441018c943a690784987e216706150483084f02
Kine Johnsrud <kinejohnsrud@gmail.com>
2016-05-16 16:17:57 +0200

    added def __str__ to the database models, compatibility with python 2, and performed CRUD operations on database  via python shell

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,28 @@
 from __future__ import unicode_literals
 
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible #making it compatible with python 2.7
+
+
+@python_2_unicode_compatible
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
+	def __str__(self):
+		return self.question_text
+	#	return "Hallo"
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+@python_2_unicode_compatible
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE) #Each choice is related to a single question
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
+	def __str__(self):
+		return self.choice_text
+		#return 'Hello'
+
