39f15e2a8bd75c5c8e5990e58f28deb85823065b
Kine Johnsrud <kinejohnsrud@gmail.com>
2016-05-24 11:23:05 +0200

    adds 404-handling to detail-view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-#from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Question #added import to access the Questions from the database/model
 
 #display the 5 latest polls questions on the index page, separated by commas, according to publication date
@@ -15,8 +14,16 @@ def index(request):
 	#return HttpResponse(template.render(context, request))
 	return render(request, 'polls/index.html', context)		#removes need for HttpResponse and loader
 
+#Raising a 404-error if question does not exist
+#Use the get_object_or_404 shortcut that Django provides
 def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+	#try:
+	#	question = Question.objects.get(pk=question_id)
+	#except Question.DoesNotExist:
+	#	raise Http404("Question does not exist")
+	question = get_object_or_404(Question, pk=question_id)	#takes Django model as first argument, and arbitrary number of keyword arguments, which it passes to the get() function of the model's manager
+	return render(request, 'polls/detail.html', {'question': question})
+	#return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
 	return HttpResponse("You're looking at the result of question %s." % question_id)
