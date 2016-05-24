fc91cfaf9a72a24fe1ad7127cb34cec96321d473
Kine Johnsrud <kinejohnsrud@gmail.com>
2016-05-24 10:47:55 +0200

    polls index site displays 5 latest polls questions according to pub_date

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question #added import to access the Questions from the database/model
 
+#display the 5 latest polls questions on the index page, separated by commas, according to publication date
 def index(request):
-	return HttpResponse("Hello, World. You're at the polls index.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	output = ', '.join([q.question_text for q in latest_question_list])
+	return HttpResponse(output)
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
