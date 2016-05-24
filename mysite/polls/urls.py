5b7a1fb8eab310627c436b752c5346ad571db454
Kine Johnsrud <kinejohnsrud@gmail.com>
2016-05-24 10:34:47 +0200

    Adds the 3 views: detail, results and vote, to the polls app, with corresponding url-patterns.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,7 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	# /polls/
 	url(r'^$', views.index, name='index'),
-	#url(r'^polls/', include('polls.urls')),
-	#url(r'^admin/', admin.site.urls),
+	# /polls/5
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	# /polls/5/results
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+	# /polls/5/results/vote
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
