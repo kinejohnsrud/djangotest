82f6cc8bf95eaf4ff110fb2620f018f8b79c72e7
Kine Johnsrud <kinejohnsrud@gmail.com>
2016-05-24 11:10:22 +0200

    adds render() shortcut, removing the need of importing HttpResponse and loader

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+#from django.template import loader
 from .models import Question #added import to access the Questions from the database/model
 
 #display the 5 latest polls questions on the index page, separated by commas, according to publication date
@@ -8,11 +8,12 @@ from .models import Question #added import to access the Questions from the data
 #the context is a dictionary mapping template variable names to Python objects
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
+	#template = loader.get_template('polls/index.html')
 	context = {'latest_question_list': latest_question_list,}
 	#output = ', '.join([q.question_text for q in latest_question_list])
 	#return HttpResponse(output)
-	return HttpResponse(template.render(context, request))
+	#return HttpResponse(template.render(context, request))
+	return render(request, 'polls/index.html', context)		#removes need for HttpResponse and loader
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
