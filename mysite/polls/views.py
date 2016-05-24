ff2adb614a703bb3f549d8cded981ecc1aa6d4ba
Kine Johnsrud <kinejohnsrud@gmail.com>
2016-05-24 11:04:03 +0200

    modifies index view to use polls/index.html template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question #added import to access the Questions from the database/model
 
 #display the 5 latest polls questions on the index page, separated by commas, according to publication date
+#loads the template called polls/index.html and passes it a context
+#the context is a dictionary mapping template variable names to Python objects
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	output = ', '.join([q.question_text for q in latest_question_list])
-	return HttpResponse(output)
+	template = loader.get_template('polls/index.html')
+	context = {'latest_question_list': latest_question_list,}
+	#output = ', '.join([q.question_text for q in latest_question_list])
+	#return HttpResponse(output)
+	return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
