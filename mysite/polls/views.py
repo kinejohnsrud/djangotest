444dc2890c20cdf81e38684871694ec9a27dfe5c
Kine Johnsrud <kinejohnsrud@gmail.com>
2016-05-25 17:42:37 +0200

    changes the views index, detail and results to use generic Django views ListView and DetailView

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,31 +1,50 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question, Choice #added import to access the Questions from the database/model
+from django.http import HttpResponseRedirect
+from .models import Question, Choice 
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 #display the 5 latest polls questions on the index page, separated by commas, according to publication date
 #loads the template called polls/index.html and passes it a context
 #the context is a dictionary mapping template variable names to Python objects
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	context = {'latest_question_list': latest_question_list,}
+#def index(request):
+	#latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	#context = {'latest_question_list': latest_question_list,}
 
-	return render(request, 'polls/index.html', context)		#removes need for HttpResponse and loader
+	#return render(request, 'polls/index.html', context)		#removes need for HttpResponse and loader
 
 #Raising a 404-error if question does not exist
 #Use the get_object_or_404 shortcut that Django provides
-def detail(request, question_id):
+#def detail(request, question_id):
 	#try:
 	#	question = Question.objects.get(pk=question_id)
 	#except Question.DoesNotExist:
 	#	raise Http404("Question does not exist")
-	question = get_object_or_404(Question, pk=question_id)	#takes Django model as first argument, and arbitrary number of keyword arguments, which it passes to the get() function of the model's manager
-	return render(request, 'polls/detail.html', {'question': question})
+	#question = get_object_or_404(Question, pk=question_id)	#takes Django model as first argument, and arbitrary number of keyword arguments, which it passes to the get() function of the model's manager
+	#return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question': question})
+#def results(request, question_id):
+	#question = get_object_or_404(Question, pk=question_id)
+	#return render(request, 'polls/results.html', {'question': question})
 	#return HttpResponse("You're looking at the result of question %s." % question_id)
+
+# --------------------------- Changing to use generic Django views ----------------------- #
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
