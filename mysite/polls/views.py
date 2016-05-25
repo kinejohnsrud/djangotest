ccf4626ff167f8d2966d2a898cedced465a3e326
Kine Johnsrud <kinejohnsrud@gmail.com>
2016-05-25 17:16:41 +0200

    adds logic to vote view and results view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Question #added import to access the Questions from the database/model
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice #added import to access the Questions from the database/model
+from django.core.urlresolvers import reverse
 
 #display the 5 latest polls questions on the index page, separated by commas, according to publication date
 #loads the template called polls/index.html and passes it a context
 #the context is a dictionary mapping template variable names to Python objects
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	#template = loader.get_template('polls/index.html')
 	context = {'latest_question_list': latest_question_list,}
-	#output = ', '.join([q.question_text for q in latest_question_list])
-	#return HttpResponse(output)
-	#return HttpResponse(template.render(context, request))
+
 	return render(request, 'polls/index.html', context)		#removes need for HttpResponse and loader
 
 #Raising a 404-error if question does not exist
@@ -23,11 +21,26 @@ def detail(request, question_id):
 	#	raise Http404("Question does not exist")
 	question = get_object_or_404(Question, pk=question_id)	#takes Django model as first argument, and arbitrary number of keyword arguments, which it passes to the get() function of the model's manager
 	return render(request, 'polls/detail.html', {'question': question})
-	#return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-	return HttpResponse("You're looking at the result of question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
+	#return HttpResponse("You're looking at the result of question %s." % question_id)
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	#request.POST raises KeyError if choice data is not provided. If error: Redisplay question with error-message
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "You didn't select a choice.",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		#HttpResponseRedirect takes a single argument, which is the URL the user is to be redirected to
+		#The reverse function will return a string like 'polls/3/results'
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) 
 
