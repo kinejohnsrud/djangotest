8e090674a1b46b8ed8a952f6e60e7e121298587c
Kine Johnsrud <kinejohnsrud@gmail.com>
2016-05-25 17:41:32 +0200

    changes URL patterns in order to use generic Django views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,11 +7,15 @@ app_name = 'polls'
 
 urlpatterns = [
 	# /polls/
-	url(r'^$', views.index, name='index'),
+	#change views from views.detail to views.DetailView.as_view() in order to use generic django views
+	#question_id --> pk
+	#url(r'^$', views.index, name='index'),
+	url(r'^$', views.IndexView.as_view(), name='index'),
 	# /polls/5
-	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	#url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 	# /polls/5/results
-	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+	url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 	# /polls/5/results/vote
 	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
